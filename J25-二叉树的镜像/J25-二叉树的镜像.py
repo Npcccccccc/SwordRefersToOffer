11c83791e20ef0d19c23260f46f473fa36924fb6
x-thon <522761613@qq.com>
2019-05-12 22:17:11 +0800

    Solve J25;

---
+++ J25-二叉树的镜像/J25-二叉树的镜像.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # 返回镜像树的根节点
+    def Mirror(self, root):
+        if root == None:
+            return 
+        if root.left == None and root.right == None:
+            return
+
+        root.left, root.right = root.right, root.left
+
+        if root.left:
+            self.Mirror(root.left)
+        if root.right:
+            self.Mirror(root.right)
